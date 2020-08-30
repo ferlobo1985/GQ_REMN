fc32224761099da40bb9b61bb4b517a11a4221ce
Fernando <dev.teach.user.dummy@gmail.com>
2020-08-30 13:31:29 -0300

    02 graphql and express

--- server/server.js
+++ server/server.js
@@ -2,13 +2,36 @@ const express = require('express');
 const app = express();
 const { graphqlHTTP } = require('express-graphql');
 const bodyParser = require('body-parser');
+const { buildSchema } = require('graphql');
+const expressPlayground = require('graphql-playground-middleware-express').default
 
 
 app.use(bodyParser.json());
+app.get('/playground',expressPlayground({endpoint: '/graphql'}))
+
 app.use(
     '/graphql',
     graphqlHTTP({
-        
+        schema:buildSchema(`
+            type RootQuery {
+                hello: String!
+            }
+
+            type RootMutation{
+                somemutation:String
+            }
+
+            schema {
+                query: RootQuery
+                mutation: RootMutation
+            }
+        `),
+        rootValue:{
+            hello:()=>{
+                return 'Hello back!!'
+            }
+        },
+        graphiql: true
     })
 )
 
